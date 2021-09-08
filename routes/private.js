941c863d0e388d980f429b48a1b97015735717d4
João Monteiro <jfcm.monteiro@gmail.com>
2021-09-08 18:09:08 +0100

    done

---
+++ routes/private.js
@@ -0,0 +1,15 @@
+const router = require("express").Router();
+
+function requireLogin(req, res, next) {
+    if(req.session.currentUser) {
+        next();
+    } else {
+        res.redirect("/login")
+    }
+}
+
+router.get("/private", requireLogin, (req, res) => {
+    res.render("private");
+});
+
+module.exports = router;
